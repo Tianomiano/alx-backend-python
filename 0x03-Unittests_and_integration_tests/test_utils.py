61c70bf971d253d25e26e9572b274b2a68a0746a
Tianomiano <patomiano6@gmail.com>
2023-10-29 17:34:29 +0300

    mock http calls and memoization unnittests

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -38,3 +38,51 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         with self.assertRaises(Exception):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """
+    test method ensures that the utils.get_json function
+    works as expected without making actual external HTTP calls,
+    verifies that mocked requests.get method is called correctly.
+    """
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    @patch("requests.get")
+    def test_get_json(self, test_url, test_payload, make_requests_get):
+        """
+        Create a Mock object for the requests.get method
+        Patch the requests.get function with the Mock object
+        """
+        make_requests_get.return_value.json.return_value = test_payload
+        result = get_json(test_url)
+        self.assertEqual(result, test_payload)
+        make_requests_get.assert_called_once_with(test_url)
+
+
+class TestMemoize(unittest.TestCase):
+    """
+    TestMemoize class, which is a test case for the
+    @memoize decorator behavior.
+    """
+    def test_memoize(self):
+        """
+        the @memoize decorator caches the result of a_method,
+        ensuring it is called only once, and that the correct
+        result is returned when accessing a_property multiple times.
+        """
+        class TestClass:
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+        with patch.object(TestClass, 'a_method') as mock_object:
+            test = TestClass()
+            test.a_property()
+            test.a_property()
+            mock_object.assert_called_once()
